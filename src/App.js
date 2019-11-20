b9f54a24fc0bad256de369ba7830467493990715
arthas168 <pavlovskipetko@abv.bg>
2019-11-20 15:11:26 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	window.Aepp.request.connect().then(result => {
+		// get the result
+		// and make action
+		console.log(result);
+	});
+
+	return (
+		<div className="App">
+			<div className="options">
+				<div className="connect-wallet">
+					<Button variant="primary">Connect Wællet</Button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
